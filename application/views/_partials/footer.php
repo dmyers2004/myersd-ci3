7791b9adf56b3e801793230f0616c64882de4720
Don Myers <myersd@trueaction.com>
2013-05-25 18:10:40 -0400

    permission change 755 to 644

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -5,5 +5,10 @@
 		<script src="/assets/js/site.js"></script>
 		<?=$page_footer ?>
 		<?=$flash_msg ?>
+		<div><small>
+			Page rendered in <strong><?php echo $this->benchmark->elapsed_time() ?></strong> seconds.
+			Environment <strong><?php echo ENVIRONMENT ?></strong>
+			Memory <strong><?php echo $this->benchmark->memory_usage() ?></strong>
+		</small></div>
 	</body>
 </html>
