c21137d2131166fde29e20c7577dd34d50d722f5
Don Myers <myersd@trueaction.com>
2013-05-31 14:20:14 -0400

    new page config - cleaner and faster

--- application/config/page.php
+++ application/config/page.php
@@ -15,39 +15,6 @@
  * body_class - class added to <body> element overwritten
  */
 
-$config['title.separator'] = ' - ';
-
-/* what view variable to attach the page partials to */
-$config['variables.title'] = 'meta_title'; /* base site title */
-$config['variables.meta'] = 'page_meta'; /* before <body> */
-$config['variables.header'] = 'page_header'; /* after <body> */
-$config['variables.container'] = 'container';
-$config['variables.footer'] = 'page_footer'; /* before </body> */
-
-/* raw string input */
-$config['base'] = array(
-	'variables.title' => 'Apple 64',
-	
-	'variables.meta' => '	<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
-		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap-responsive.min.css">
-		<link rel="stylesheet" href="/assets/fontawesome/css/font-awesome.min.css">
-		<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">    
-	  <script src="/assets/js/modernizr-2.6.2.min.js"></script>',
-	
-	'variables.header' => '<!--[if lt IE 8]>
-		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-		<![endif]-->',
-	
-	'variables.footer' => '<script src="/assets/jquery/jquery-1.9.1.min.js"></script>
-		<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
-		<script src="/assets/js/plugins.js">
-		</script><script src="/assets/js/site.js"></script>'
-);
-
 /*
 we can run php here - while not "recommended" in this case I think it fits the task
 also this could be directly in the function call below but I wanted to grab the separately.
@@ -63,56 +30,90 @@ if ($roles === null) {
 }
 */
 
-/* default set */
-$config['default']['js'] = array();
-$config['default']['css'] = array();
-$config['default']['meta'] = array();
-$config['default']['data'] = array(
+/* Map Common Names to Custom View Variables */
+$config['variable_mappings'] = array(
+	'title' => 'meta_title', /* base site title */
+	'meta' => 'page_meta', /* before <body> */
+	'header' => 'page_header', /* after <body> */
+	'bodyClass' => 'page_body_class', /* class attached to the <body class="#"> */
+	'container' => 'container',
+	'footer' => 'page_footer', /* before </body> */
+	'pageBrand' => 'page_brand' /* Brand - Title */
+);
+
+/*
+    ___      __             _ _   
+   /   \___ / _| __ _ _   _| | |_ 
+  / /\ / _ \ |_ / _` | | | | | __|
+ / /_//  __/  _| (_| | |_| | | |_ 
+/___,' \___|_|  \__,_|\__,_|_|\__|
+
+*/
+$config['default'] = array(
+	'$template' 				=> '_templates/default',
+	'title' 						=> 'Apple 64',
+	'pageBrand'					=> 'GTags',
+
+	'meta' => '<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap-responsive.min.css">
+		<link rel="stylesheet" href="/assets/fontawesome/css/font-awesome.min.css">
+		<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">    
+	  <script src="/assets/js/modernizr-2.6.2.min.js"></script>',
+
+	 'header' => '<!--[if lt IE 8]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p><![endif]-->',
+
+	 'footer' => '<script src="/assets/jquery/jquery-1.9.1.min.js"></script>
+			<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+			<script src="/assets/js/plugins.js"></script>
+			<script src="/assets/js/site.js"></script>'
+);
+
+/*
+   ___       _     _ _     
+  / _ \_   _| |__ | (_) ___ 
+ / /_)/ | | | '_ \| | |/ __|
+/ ___/| |_| | |_) | | | (__ 
+\/     \__,_|_.__/|_|_|\___|
+                            
+*/
+$config['public'] = array(
+	'bodyClass'			 			=> '$ public',
 	'foo'									=> 'bar',
 	'logged_in' 					=> get_instance()->auth->is_logged_in(),
 	'navigation_menu' 		=> get_instance()->menubar->render($roles,$menu),
-	'page_body_class'			=> 'default',
-	'page_brand'					=> 'GTags'
-);
-$config['default']['template'] = '_templates/default';
-$config['default']['assets'] = '/assets/';
-$config['default']['folder'] = '';
-
-
-/* public group */
-$config['public']['css'] = array(
-	'css/template.css',
-	'css/style.css'
-);
-$config['public']['js'] = array(
-	'js/onready.js'
-);
-$config['public']['data'] = array(
-	'page_body_class' => 'public'
+	'meta'								=> '$<link rel="stylesheet" href="/assets/css/template.css">
+															<link rel="stylesheet" href="/assets/css/style.css">',
+	'footer'							=> '$<script src="/assets/js/onready.js"></script>'
 );
 
-
-/* admin group */
-$config['admin']['css'] = array(
-	'css/template.css',
-	'css/style.css',
-	'admin/css/admin.css',
-	'chosen/chosen.css',
-	'table-fixed-header/table-fixed-header.css'
+/*
+   _       _           _       
+  /_\   __| |_ __ ___ (_)_ __  
+ //_\\ / _` | '_ ` _ \| | '_ \ 
+/  _  \ (_| | | | | | | | | | |
+\_/ \_/\__,_|_| |_| |_|_|_| |_|
+                             
+*/
+$config['admin'] = array(
+	'$template' 					=> 'admin/_templates/default',
+	'title'								=> '$ - Admin',
+	'bodyClass'			 			=> '$ admin',
+	'meta'								=> '$<link rel="stylesheet" href="/assets/css/template.css">
+															<link rel="stylesheet" href="/assets/css/style.css">
+															<link rel="stylesheet" href="/assets/admin/css/admin.css">
+															<link rel="stylesheet" href="/assets/chosen/chosen.css">
+															<link rel="stylesheet" href="/assets/table-fixed-header/table-fixed-header.css">',
+	'footer'							=> '$<script src="/assets/chosen/chosen.jquery.min.js"></script>
+															<script src="/assets/table-fixed-header/table-fixed-header.js"></script>
+															<script src="/assets/admin/js/jquery.ajax.form.js"></script>
+															<script src="/assets/admin/js/jquery.ajax.link.js"></script>
+															<script src="/assets/admin/js/jquery.combobox.js"></script>
+															<script src="/assets/admin/js/jquery.filter_input.js"></script>
+															<script src="/assets/admin/js/admin_onready.js"></script>'
 );
-$config['admin']['js'] = array(
-	'chosen/chosen.jquery.min.js',
-	'table-fixed-header/table-fixed-header.js',
-	'admin/js/jquery.ajax.form.js',
-	'admin/js/jquery.ajax.link.js',
-	'admin/js/jquery.combobox.js',
-	'admin/js/jquery.filter_input.js',
-	'admin/js/admin_onready.js'
-);
-$config['admin']['data'] = array(
-	'page_body_class' => 'admin'
-);
-$config['admin']['template'] = 'admin/_templates/default';
-$config['admin']['title'] = 'Admin';
 
 /* additional groups below */
--- application/libraries/Page.php
+++ application/libraries/Page.php
@@ -2,125 +2,90 @@
 
 class Page
 {
-  public $config = array(); /* all config */
-  public $settings = array('js'=>array(),'css'=>array(),'meta'=>array(),'data'=>array()); /* array of current settings */
+  public $config = array(); /* all configs local cache */
 
-	public $template = ''; /* template to use on build */
-	public $folder = ''; /* folder to look in for all page view files */
-	public $assets = '';
-	public $separator = ' / ';
+	public $template = ''; /* template to use in build method */
 
 	public $default_css = array('rel'=>'stylesheet','type'=>'text/css','href'=>'');
 	public $default_js = array('src'=>'');
 	public $default_meta = array('name'=>'','content'=>'');
+	
+	/* css where, js where */
 
   public function __construct()
   {
 		/* load using the magic settings file -> database */
     $this->config = $this->load->settings('page');
 
-		$this->separator = $this->config['title.separator']
-
-		/* raw base value (strings) injected into view variables */
-		$this->base();
-
 		/* load in our defaults */
 		$this->load('default');
   }
-
-	private function base() {
-		foreach ($this->config['base'] as $key => $value) {
-			$this->setVar($this->config[$key],$value);
-		}
-	}
-
+  
 	public function load($grouping)
 	{
-		$group = $this->config[$grouping];
-		
-		foreach ((array)$group['meta'] as $value) {
-			$this->meta($value);
+		foreach ($this->config[$grouping] as $key => $value) {
+			$this->add($key,$value);
 		}
-
-		foreach ((array)$group['css'] as $value) {
-			$this->link($value);
-		}
-
-		foreach ((array)$group['js'] as $value) {
-			$this->script($value);
-		}
-
-		foreach ((array)$group['data'] as $key => $value) {
-			$this->setVar($key,$value);
-		}
-
-		/* append any new title sections */
-		$this->title($group['title']);
-
-		/* change the template and folders if new values are present */
-		$this->template = $this->getDefault($group['template'],$this->template);
-		$this->folder = $this->getDefault($group['folder'],$this->folder);
-		$this->assets = $this->getDefault($group['assets'],$this->assets);
 
 		return $this;
 	}
 
-  public function css($href='',$additional_attributes=array(),$after='after')
+	public function add($key,$value) {
+		/* if the key starts with $ then it's applied to $this (page object) */
+		if ($key{0} == '$') {
+			$key = substr($key,1);
+			$this->$key = $value;
+		} else {
+			/* if it's not then apply it to a view variable */
+			$where = $value{0};
+			switch ($where) {
+				case '^': /* append to current view variable */
+					$value = substr($value,1);
+					$where = 'before';
+				break;
+				case '$': /* prepend to current view variable */
+					$value = substr($value,1);
+					$where = 'after';
+				break;
+				default: /* replace what's currently in view variable */
+					$where = 'overwrite';
+			}
+			
+			/* this will try to do a mapping if a exists in $config['variables'][%key%]*/
+			$this->_add($key,$value,$where);
+		}
+	}
+
+  public function css($href='',$additional_attributes=array(),$where='after')
   {
-    return $this->link($href,$additional_attributes,$after);
+    return $this->link($href,$additional_attributes,$where);
   }
 
-  public function link($href='',$additional_attributes=array(),$after='after')
+  public function link($href='',$additional_attributes=array(),$where='after')
   {
     $merged = (is_array($href)) ? $href : array_merge($this->default_css,array('href'=>$this->_prefix($href)),$additional_attributes);
-    $this->add('meta','<link '.$this->_ary2attr($merged).' />',$after);
+    $this->_add('meta','<link '.$this->_ary2attr($merged).' />',$where);
 
     return $this;
   }
 
-  public function js($file='',$additional_attributes=array(),$after='after')
+  public function js($file='',$additional_attributes=array(),$where='after')
   {
-    return $this->script($file,$additional_attributes,$after);
+    return $this->script($file,$additional_attributes,$where);
   }
 
-  public function script($file='',$additional_attributes=array(),$after='after')
+  public function script($file='',$additional_attributes=array(),$where='after')
   {
     $merged = (is_array($file)) ? $file : array_merge($this->default_js,array('src'=>$this->_prefix($file)),$additional_attributes);
-    $this->add('footer','<script '.$this->_ary2attr($merged).'></script>',$after);
+    $this->_add('footer','<script '.$this->_ary2attr($merged).'></script>',$where);
 
     return $this;
   }
 
-  public function meta($name='',$content='',$additional_attributes=array(),$after='after')
+  public function meta($name='',$content='',$additional_attributes=array(),$where='after')
   {
     $merged = (is_array($name)) ? $name : array_merge($this->default_meta,array('name'=>$name,'content'=>$content),$additional_attributes);
-    $this->add('meta','<meta '.$this->_ary2attr($merged).'>',$after);
-
-    return $this;
-  }
-
-  /* append onto current title */
-  public function title($title=null)
-  {
-		if ($title) {
-			$this->add('title',$this->separator.$title);
-		}
-
-    return $this;
-  }
-
-	/* add body class */
-  public function addBodyClass($class=null)
-  {
-		$this->add('page_body_class',$class);
-		
-    return $this;
-  }
-
-	/* change the title separator */
-  public function separator($separator=null)
-  {
-    $this->separator = $separator;
+    $this->_add('meta','<meta '.$this->_ary2attr($merged).'>',$where);
 
     return $this;
   }
@@ -132,48 +97,12 @@ class Page
 
     return $this;
   }
-
-  /* change the view look up folder */
-	public function folder($folder=null)
-	{
-		$this->folder = $folder;
-
-		return $this;
-	}
-	
-	/* change the default assets folder (off web root) */
-	public function assets($folder=null)
-	{
-		$this->assets = $folder;
-
-		return $this;
-	}
-
-	/* wrapper for load partial */
-	public function partial($view,$data=array(),$name=null)
-	{
-		return $this->load->partial($this->folder.$view,$data,$name);
-	}
-
-	/* final output */
-  public function build($view=null,$layout=null)
-  {		
-		/* while the dynamic view finder is uber cool it does take a bit longer to process */
-		$view = ($view) ? $view : trim($this->router->fetch_directory().str_replace('Controller','',$this->router->fetch_class()).'/'.str_replace('Action','',$this->router->fetch_method()),'/');
-
-		$this->setVar($this->config['variables.container'],$this->load->partial($this->folder.$view));
-
-    /* final output */
-    $this->load->view((($layout) ? $layout : $this->folder.$this->template),null,false);
-
-		return $this;
-	}
-
+  
 	/* Add to meta, header, footer before or after what already in there */
-	public function add($which='',$what=null,$where='after')
+	private function _add($which='',$what='',$where='after')
 	{
 		if (!empty($what)) {
-			$var = $this->getDefault($this->config['variables.'.$which],$which);
+			$var = $this->getDefault($this->config['variable_mappings'][$which],$which);
 	
 			switch ($where) {
 				case 'before':
@@ -199,6 +128,26 @@ class Page
 		return $this->setVar($name,$value);
 	}
 
+	/* wrapper for load partial */
+	public function partial($view,$data=array(),$name=null)
+	{
+		return $this->load->partial($this->folder.$view,$data,$name);
+	}
+
+	/* final output */
+  public function build($view=null,$layout=null)
+  {		
+		/* while the dynamic view finder is uber cool it does take a bit longer to process */
+		$view = ($view) ? $view : trim($this->router->fetch_directory().str_replace('Controller','',$this->router->fetch_class()).'/'.str_replace('Action','',$this->router->fetch_method()),'/');
+
+		$this->setVar($this->config['variable_mappings']['container'],$this->load->partial($this->folder.$view));
+
+    /* final output */
+    $this->load->view((($layout) ? $layout : $this->folder.$this->template),null,false);
+
+		return $this;
+	}
+	
   private function _ary2attr($array)
   {
     $output = '';
