55f49e9f3783e343bc3cf0e18853e75908fab3e5
Don Myers <myersd@trueaction.com>
2013-06-02 20:17:14 -0400

    database session driver

--- application/config/config.php
+++ application/config/config.php
@@ -295,7 +295,7 @@ $config['encryption_key'] = '28c37869df0d7eb9e2d9138d72b76cc7';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_driver']			= 'native';
+$config['sess_driver']			= 'database';
 $config['sess_valid_drivers']	= array('database');
 $config['sess_cookie_name']		= 'session_cookie';
 $config['sess_expiration']		= 7200;
