6f698cf7d142888435d1fc323e23b07454844b34
Don Myers <myersd@trueaction.com>
2013-06-15 14:28:36 -0400

    update nav

--- application/views/admin/menubar/form.php
+++ application/views/admin/menubar/form.php
@@ -6,9 +6,13 @@ $crud->form_bs_basic('Text*',form_text('text',$record->text,'input-small','Displ
 $crud->form_bs_basic('Resource*',form_text('resource',$record->resource,'input-xlarge','/nav/...'));
 $crud->form_bs_basic('URL',form_text('url',$record->url,'input-xlarge','/'));
 $crud->form_bs_basic('Class',form_text('class',$record->class));
+
 if ($record->id == -1) {
 	$crud->form_bs_basic('Parent Menu',form_dropdown('parent_id', $options, $record->parent_id, 'class="chosen"'));
+} else {
+	echo '<input type="hidden" name="parent_id" value="'.$record->parent_id.'">';
 }
+
 $crud->form_bs_basic('Active',form_checkbox('active', 1, $record->active));
 
 echo '<input type="hidden" name="sort" value="'.$record->sort.'">';
