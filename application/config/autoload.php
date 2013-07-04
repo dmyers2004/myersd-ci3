5f1c1a4b212a1908d623e51c2f9753b621c099d4
Don Myers <myersd@trueaction.com>
2013-07-04 09:40:51 -0400

    add data helper to application helper this way I don't have a bunch of required helpers everything can go in application helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -107,7 +107,7 @@ $autoload['drivers'] = array('session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','data');
+$autoload['helper'] = array('url','application');
 
 /*
 | -------------------------------------------------------------------
